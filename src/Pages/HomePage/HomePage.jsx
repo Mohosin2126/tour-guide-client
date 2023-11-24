a12afd74b8913bcf2e1fab0d645bf57918a74bcd
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-24 18:38:58 +0800

    crated home pages and error page and set it's route

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>hello this is me home </h1>
+        </div>
+    );
+};
+
+export default HomePage;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,10 +2,20 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import HomePage from "../Pages/HomePage/HomePage";
 
  export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Layout></Layout>,
+      errorElement:<ErrorPage></ErrorPage>,
+      children:[
+        {
+path:"/",
+element:<HomePage></HomePage>
+        },
+      ]
     },
   ]);
