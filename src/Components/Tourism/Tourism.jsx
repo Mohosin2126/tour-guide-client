f2401c62fce52e5f09775b1db438f2f77b33c514
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-25 03:00:39 +0800

    modify json data

---
+++ src/Components/Tourism/Tourism.jsx
@@ -0,0 +1,11 @@
+
+const Tourism = () => {
+    return (
+        <div className="mt-5">
+            <h1 className="font-serif text-center  font-bold text-3xl">Tourism <span className="text-green-600 text-3xl">&</span> Travel Guide</h1>
+            <p className="text-sm text-center mt-2 font-mono">Explore the world with our expert travel guides, dedicated to crafting personalized journeys filled with cultural richness and unforgettable moments. <br /> Trust us to turn your travel dreams into extraordinary adventures</p>
+        </div>
+    );
+};
+
+export default Tourism;
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import Banner from "../../Components/Banner/Banner";
-import Tourism from "../../Components/Banner/Tourism";
-
+import Tourism from "../../Components/Tourism/Tourism";
 
 const HomePage = () => {
    
