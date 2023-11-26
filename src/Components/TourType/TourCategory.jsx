c89b87ecb1afd4ad76ec4650efd0fc3c2b481840
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-26 21:57:39 +0800

    used swiper js and showed the data of tour type

--- src/Components/TourType/TourCategory.jsx
+++ src/Components/TourType/TourCategory.jsx
@@ -1,11 +1,39 @@
 import { Link } from "react-router-dom";
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 const TourCategory = ({tourcategory}) => {
     const{category,image}=tourcategory
     return (
         <div>
-            
+            <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+
+{
+
+}
+
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+      </Swiper>
            <Link>
 
 <div className="card md:w-96 h-56 bg-base-100 shadow-xl image-full">
--- src/Components/TourType/TourType.jsx
+++ src/Components/TourType/TourType.jsx
@@ -1,17 +1,55 @@
 import useCategory from "../../Hook/useCategory";
-import TourCategory from "./TourCategory";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { Link } from "react-router-dom";
+
 
 const TourType = () => {
-    const [categories]=useCategory([])
+    const [categories] = useCategory([])
     console.log(categories)
     return (
         <div className="mt-10">
             <h1 className="font-serif text-center  font-bold text-3xl">Tour Type</h1>
             <p className="text-center font-semibold">Everything You Want Is Here</p>
-            <div className="flex flex-wrap justify-center mt-6 gap-5">
-                {
-                    categories.map(tourcategory=><TourCategory tourcategory={tourcategory} key={tourcategory.id} ></TourCategory>)
-                }
+            <div  className="mt-10">
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={'auto'}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 1000,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
+                    pagination={true}
+                    modules={[EffectCoverflow, Pagination]}
+                    className="mySwiper"
+                >
+                    {
+                        categories.map(tourcategory => <SwiperSlide key={tourcategory.id}>
+                            <Link>
+                               <div className="flex justify-center gap-5">
+                              
+           <div className="card md:w-1/2 h-56 bg-base-300 shadow-xl image-full">
+  <figure><img className="w-full p-1" src={tourcategory.image} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{tourcategory.category}</h2>
+  </div>
+</div>
+
+                               </div>
+                            </Link>
+
+                        </SwiperSlide>
+                          ) }
+                </Swiper>
+
             </div>
         </div>
     );
