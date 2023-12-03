535a34e78feff05209ea49c09f9c08231d6ee387
Mohosin212601~ <mohosin2126@gmail.com>
2023-12-04 01:52:57 +0800

    edited my profile photo

--- src/Pages/DashBoard/Tourist/MyProfile/MyProfile.jsx
+++ src/Pages/DashBoard/Tourist/MyProfile/MyProfile.jsx
@@ -6,7 +6,6 @@ import { AuthContext } from "../../../AuthProvider/AuthProvider";
 
 const MyProfile = () => {
     const {user}=useContext(AuthContext)
-    console.log(user.email)
     const axiosSecure=useAxiosSecure()
     const {data:users=[]}=useQuery({
         queryKey:["users",user?.email],
@@ -16,22 +15,24 @@ const MyProfile = () => {
     
         }
     })
-    const userData=users.find(user=>user.email === user.email)
+    const userData=users.find(user=>user?.email === user?.email)
     console.log(userData)
     return (
         <div>
-            <h1>My Profile</h1>
-           <div className="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+            <h1 className="text-3xl text-center">My Profile</h1>
+        <div className="flex justify-center items-center">
+        <div className=" mt-10 relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
       <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl h-80">
         <img src={userData?.photo} className="w-full h-52" alt="profile-picture" />
       </div>
       <div className="p-6 text-center">
-        <h4 className="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+        <h4 className="uppercase  block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
       {userData?.name}
         </h4>
-     <h2>{userData?.role}</h2>
+     <h2 className="uppercase">{userData?.role}</h2>
       </div>
     </div>
+        </div>
         </div>
     );
 };
