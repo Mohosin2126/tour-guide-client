6c72e790ada352c8f017c5e36ff48a48108e35ea
Mohosin212601~ <mohosin2126@gmail.com>
2023-12-03 15:15:01 +0800

    banner changes

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -48,71 +48,72 @@ const Banner = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-     
-          <div className="h-screen  relative">
-            <img className="h-screen w-screen" src="https://i.ibb.co/Y29WPDw/Sajek-Valley-20161205.jpg" alt="" />
-            <div className="absolute inset-0 mt-56 md:mt-48">
-             <div data-aos="fade-right">
+        <div>
+            <div className="hero h-screen" style={{backgroundImage: 'url(https://i.ibb.co/Y29WPDw/Sajek-Valley-20161205.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div >
+    <div data-aos="fade-right">
              <h1 className="text-center font-serif md:font-extrabold md:text-4xl text-2xl text-white">Your Gateway to Extraordinary Travel 
               </h1>
              </div>
-              <div data-aos="fade-left">
+
+             <div data-aos="fade-left">
               <p className="text-center mt-2 md:font-semibold md:text-base text-sm text-stone-300">Welcome to Your Gateway to Extraordinary Travel, where every journey is a carefully curated experience crafted just for you. <br />Embark on a seamless adventure as our expert tour guides lead you through captivating destinations, unveiling hidden gems and cultural treasures</p>
               <button className=" mt-5 flex mx-auto bg-transparent hover:bg-red-600 text-red-600 font-serif font-semibold hover:text-white py-1 md:py-2 px-2 md:px-4 border  hover:border-transparent rounded">
  Continue Reading 
 </button>
               </div>
-            
-             <div>
-             
-             </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-        <div className="h-screen  relative">
-            <img className="h-screen w-screen" src="https://i.ibb.co/Zm6FDLD/top-ten-tourist-places-to-visit-in-bangladesh-1.webp" alt="" />
-            <div className="absolute inset-0 mt-56 md:mt-48">
-             <div data-aos="fade-right">
-             <h1 className="text-center font-serif md:font-extrabold md:text-4xl text-2xl text-white">Your Gateway to Extraordinary Travel 
-              </h1>
-             </div>
-              <div data-aos="fade-left">
-              <p className="text-center mt-2 md:font-semibold md:text-base text-sm text-stone-300">Welcome to Your Gateway to Extraordinary Travel, where every journey is a carefully curated experience crafted just for you. <br />Embark on a seamless adventure as our expert tour guides lead you through captivating destinations, unveiling hidden gems and cultural treasures</p>
-              <button className=" mt-5 flex mx-auto bg-transparent hover:bg-red-600 text-red-600 font-serif font-semibold hover:text-white py-1 md:py-2 px-2 md:px-4 border  hover:border-transparent rounded">
- Continue Reading 
-</button>
-           
-              </div>
-            
-             <div>
-             
-             </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-        <div className="h-screen  relative">
-            <img className="h-screen w-screen" src= "https://i.ibb.co/Sn33hpM/Kuakata-beach.jpg" alt="" />
-            <div className="absolute inset-0 mt-56 md:mt-48">
-             <div data-aos="fade-right">
-             <h1 className="text-center font-serif md:font-extrabold md:text-4xl text-2xl text-white">Your Gateway to Extraordinary Travel 
-              </h1>
-             </div>
-              <div data-aos="fade-left">
-              <p className="text-center mt-2 md:font-semibold md:text-base text-sm text-stone-300">Welcome to Your Gateway to Extraordinary Travel, where every journey is a carefully curated experience crafted just for you. <br />Embark on a seamless adventure as our expert tour guides lead you through captivating destinations, unveiling hidden gems and cultural treasures</p>
-              <button className=" mt-5 flex mx-auto bg-transparent hover:bg-red-600 text-red-600 font-serif font-semibold hover:text-white py-1 md:py-2 px-2 md:px-4 border  hover:border-transparent rounded">
- Continue Reading 
-</button>
-           
-              </div>
-            
-             <div>
-             
-             </div>
-            </div>
-          </div>
+    </div>
+  </div>
+</div>
+        </div>
          
+        </SwiperSlide>
+        <SwiperSlide>
+        <div>
+            <div className="hero h-screen" style={{backgroundImage: 'url(https://i.ibb.co/Zm6FDLD/top-ten-tourist-places-to-visit-in-bangladesh-1.webp)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div >
+    <div data-aos="fade-right">
+             <h1 className="text-center font-serif md:font-extrabold md:text-4xl text-2xl text-white">Your Gateway to Extraordinary Travel 
+              </h1>
+             </div>
+
+             <div data-aos="fade-left">
+              <p className="text-center mt-2 md:font-semibold md:text-base text-sm text-stone-300">Welcome to Your Gateway to Extraordinary Travel, where every journey is a carefully curated experience crafted just for you. <br />Embark on a seamless adventure as our expert tour guides lead you through captivating destinations, unveiling hidden gems and cultural treasures</p>
+              <button className=" mt-5 flex mx-auto bg-transparent hover:bg-red-600 text-red-600 font-serif font-semibold hover:text-white py-1 md:py-2 px-2 md:px-4 border  hover:border-transparent rounded">
+ Continue Reading 
+</button>
+              </div>
+    </div>
+  </div>
+</div>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+       
+        <div>
+            <div className="hero h-screen" style={{backgroundImage: 'url(https://i.ibb.co/Sn33hpM/Kuakata-beach.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div >
+    <div data-aos="fade-right">
+             <h1 className="text-center font-serif md:font-extrabold md:text-4xl text-2xl text-white">Your Gateway to Extraordinary Travel 
+              </h1>
+             </div>
+
+             <div data-aos="fade-left">
+              <p className="text-center mt-2 md:font-semibold md:text-base text-sm text-stone-300">Welcome to Your Gateway to Extraordinary Travel, where every journey is a carefully curated experience crafted just for you. <br />Embark on a seamless adventure as our expert tour guides lead you through captivating destinations, unveiling hidden gems and cultural treasures</p>
+              <button className=" mt-5 flex mx-auto bg-transparent hover:bg-red-600 text-red-600 font-serif font-semibold hover:text-white py-1 md:py-2 px-2 md:px-4 border  hover:border-transparent rounded">
+ Continue Reading 
+</button>
+              </div>
+    </div>
+  </div>
+</div>
+        </div>
         </SwiperSlide>
       </Swiper>
       <div >
