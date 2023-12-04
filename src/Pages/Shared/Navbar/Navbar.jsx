bbbd3a634d19d2df106db28042de3bfa033ba1bc
Mohosin212601~ <mohosin2126@gmail.com>
2023-12-04 14:17:50 +0800

    navbar mmodified

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-end">
-  <div className="dropdown dropdown-bottom flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+  <div className="dropdown dropdown-left  items-center md:order-2   rtl:space-x-reverse">
     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
       <div className="w-10 rounded-full">
         {user ? (
@@ -66,12 +66,12 @@ const Navbar = () => {
     </label>
     <ul
       tabIndex={0}
-      className="mt-3 z-[1] text-start shadow menu menu-lg dropdown-content bg-base-100 rounded-box w-48"
+      className="mt-3 z-[1] text-start shadow  dropdown-content space-y-1 bg-base-100 rounded-box w-48"
     >
       <Link to={"/dashboard/myprofile"}>
-        <li  className="font-base text-center text-base">Dashboard</li>
+        <li ><button  className=" mt-5 flex mx-auto bg-transparent hover:bg-red-600 text-red-600 font-serif font-semibold hover:text-white py-1 md:py-2 px-2 md:px-4 border  hover:border-transparent rounded"> Dashboard</button></li>
       </Link>
-      <li  className="font-base text-center text-base">Offer Announcements</li>
+      <li  className="font-base  text-base">Offer Announcements</li>
       <li>
         <h1 className="font-base text-base">
           {user ? user?.displayName : "Your Name"}
@@ -85,7 +85,7 @@ const Navbar = () => {
       {user ? (
         <button
           onClick={handleLogOut}
-          className="font-base text-base"
+          className="font-base flex mx-auto font-serif text-lg text-rose-600"
         >
           LogOut
         </button>
