4dc318304906bd7fc380f2e61de3872ddad84952
Mohosin212601~ <mohosin2126@gmail.com>
2023-12-25 15:32:50 +0800

    update dist file

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -6,9 +6,7 @@ const ContactUs = () => {
             <div>
         <h1 className="text-center font-serif text-3xl text-extrabold mt-10">Contact Us</h1>
         <p className="text-center mt-2 font-serif  text-base">For Further Inquiry</p>
-        <div>
-            
-        </div>
+       
     </div>
         </div>
     );
